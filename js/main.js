c45c5099fffa64f5fcb67460b856489fb9cd7436
alvarogarcia1010 <00111317@uca.edu.sv>
2019-02-17 16:37:43 -0600

    creando rectangulos dinamicamente

--- js/main.js
+++ js/main.js
@@ -1,23 +1,54 @@
+var newRectObject, newRectObject2;
+
 $(document).ready(function(){
-  // if (SVG.supported) {
-  //   var draw = SVG('drawing').width('100%').height('100%');
-  //   var rect =draw.rect(100, 100).fill('#f06').move(20, 20)
-  //   var rect2 = draw.rect(100, 100).fill('#f09').move(40, 40)
-  //   var text = draw.plain('Lorem ipsum dolor sit amet consectetur.')
-  //   var defs = draw.defs()
-  //   var image = defs.image('https://storage.googleapis.com/cifco-bucket/organizations/1/ejemplo_pabellon_feria.png', 800,400)
-  // } else {
-  //   alert('SVG not supported')
-  // }
-  $('[data-toggle="tooltip"]').tooltip()
 
-  $("#svg-img").on("click", ".rect", function(e){
+  $('img#photo').imgAreaSelect({
+      handles: true,
+      onSelectEnd:  function (img, selection) {
+        $('input[name="x1"]').val(selection.x1);
+        $('input[name="y1"]').val(selection.y1);
+        $('input[name="x2"]').val(selection.x2);
+        $('input[name="y2"]').val(selection.y2);
+      }
+  });
+
+  $('#agregar').click(function(e){
     e.preventDefault();
+
+    newRectObject = createRect($('input[name="x1"]').val(), $('input[name="y1"]').val(), $('input[name="x2"]').val(), $('input[name="y2"]').val(), $('input[name="name"]').val(), $('input[name="color"]').val());
+
+    newRectObject2 = createRect($('input[name="x1"]').val(), $('input[name="y1"]').val(), $('input[name="x2"]').val(), $('input[name="y2"]').val(), $('input[name="name"]').val(), $('input[name="color"]').val());
+
+    $('#rect-group').append(newRectObject);
+    $('#rect-group-2').append(newRectObject2);
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $(".rect").on('click',function(){
+      $('#myModal').modal('show');
+    });
+  });
+
+  $('[data-toggle="tooltip"]').tooltip();
+
+  $(".rect").on('click',function(){
     $('#myModal').modal('show');
   });
 
-  $("#svg-img-2").on("click", ".rect", function(e){
-    e.preventDefault();
-    $('#myModal').modal('show');
-  });
 });
+
+
+function createRect(x1,y1,x2,y2,name,color){
+  var rect = document.createElementNS("http://www.w3.org/2000/svg","rect");
+  rect.setAttribute("class", "rect");
+  rect.setAttribute("x",x1);
+  rect.setAttribute("y",y1);
+  rect.setAttribute("data-Name",name);
+  rect.setAttribute("data-tabindex",1);
+  rect.setAttribute("style",`fill: ${color}; stroke: green; stroke-width: 1mm; width:${x2-x1}; height:${y2-y1}`);
+  rect.setAttribute("data-toggle", "tooltip");
+  rect.setAttribute("data-placement", "top");
+  rect.setAttribute("title", name);
+
+  return rect;
+}
