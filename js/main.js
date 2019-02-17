55707fbb88852d5f239b9c0a0e5beca2b82a7330
alvarogarcia1010 <00111317@uca.edu.sv>
2019-02-16 23:18:38 -0600

    Proyecto base

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  // if (SVG.supported) {
+  //   var draw = SVG('drawing').width('100%').height('100%');
+  //   var rect =draw.rect(100, 100).fill('#f06').move(20, 20)
+  //   var rect2 = draw.rect(100, 100).fill('#f09').move(40, 40)
+  //   var text = draw.plain('Lorem ipsum dolor sit amet consectetur.')
+  //   var defs = draw.defs()
+  //   var image = defs.image('https://storage.googleapis.com/cifco-bucket/organizations/1/ejemplo_pabellon_feria.png', 800,400)
+  // } else {
+  //   alert('SVG not supported')
+  // }
+});
